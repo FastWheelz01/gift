36c7c35b5249009338544384cc5dd9082ff43028
elliot <guguslevrais@gmail.com>
2026-08-04 23:09:41 +0200

    Initial commit

---
+++ gift/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main className="flex-1 bg-linear-to-br from-indigo-500 via-purple-500 to-pink-500 text-center">
+      <div>
+        this is a page for a gift to a lovely person
+      </div>
+    </main>
+  );
+}
