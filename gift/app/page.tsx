c5b62fa1a569d4ea257204bb0e26e3f72c2a7ef1
fastwheel01 <guguslevrais@gmail.com>
2026-08-05 22:47:01 +0200

    update

--- gift/app/page.tsx
+++ gift/app/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
     <main className="gradient-pattern min-h-screen flex items-center justify-center">
       <div className="text-red-500 text-center">
         EROR-404:GIFT NOT PREPARED
-        <br />
-        FIX:COME BACK LATER (dont be impatient)
+        ETA: ~1 WEEK
       </div>
     </main>
   );
