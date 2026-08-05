3adeb251133d8b8470c29d1134d345436e6cf0ef
fastwheel01 <guguslevrais@gmail.com>
2026-08-05 22:11:10 +0200

    update

--- gift/app/page.tsx
+++ gift/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main className="flex-1 bg-linear-to-br from-indigo-500 via-purple-500 to-pink-500 text-center">
-      <div>
+      <div className="text-red-500">
         EROR-404:GIFT NOT PREPARED
         FIX:COME BACK LATER 
       </div>
