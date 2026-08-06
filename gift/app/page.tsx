ecd92934bf80a11a2f68273f056f00b6ecff603c
fastwheelz <guguslevrais@gmail.com>
2026-08-06 13:45:32 +0200

    update

--- gift/app/page.tsx
+++ gift/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="gradient-pattern min-h-screen flex items-center justify-center">
       <div className="text-red-500 text-center">
         EROR-404:GIFT NOT PREPARED
-        ETA: ~1 WEEK
+        ETA: ~1 WEEK OR MORE
       </div>
     </main>
   );
