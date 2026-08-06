2a2af9b685be3e1f40497068d860904b25d52155
fastwheel01 <guguslevrais@gmail.com>
2026-08-06 23:05:37 +0200

    update

--- gift/app/page.tsx
+++ gift/app/page.tsx
@@ -6,14 +6,14 @@ export default function Home() {
   return (
 <main className="gradient-pattern min-h-screen flex flex-col items-center justify-center text-center">
   <div className="text-red-500 text-center">
-    <h3 className="font-bold text-white drop-shadow-lg">
+    <h1 className="font-bold text-white drop-shadow-lg text-xl">
 Hello alin....it has been a lot of time since we've met each other.
       <br />
 I know this might be a small suprise to you but it has came from all my heart to make you happy and steal your heart :3
       <br />
 I shall let you acces our souvenir vault of all our fun moments and lovely text of our relation may love be with us 😘💕 ^^      
     
-    </h3>
+    </h1>
         <div className="text-center">
         <Link href="/gift">
         <button type="button" className="bg-white text-purple-700 font-semibold py-2 px-4 rounded-lg m-2">
